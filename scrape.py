6d667afa3a9b9876a78be8055f20e9128fc074fc
beinhartdavid <beinhart.david@gmail.com>
2020-06-18 16:16:09 -0400

    Initial project outline

---
+++ scrape.py
@@ -0,0 +1,28 @@
+import selenium
+
+base_url = "https://www.parktool.com/category/tools"
+
+# find element by class name = inline-headline
+
+"""
+Step 1. 
+Scrape the 'base_url', and generate a 2 dimensional list containing the url and category name of every category 
+Example lst = [ [Brakes, brakeUrl], []...]
+
+"""
+
+"""
+Step 2.
+Iterate through each URL from the list above and gather information on each product 
+"""
+
+"""
+Step 3. 
+Write information from step 2 to an Excel file. 
+"""
+
+"""
+Step 4. 
+
+Iterate the above steps for all products in all categories
+"""
